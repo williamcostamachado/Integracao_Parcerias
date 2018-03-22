6a0d7ad6359371ade65a6182faf0dfa9e91dc4e8
William Machado Costa <willian.costa@cappta.com.br>
2018-03-21 23:06:10 -0300

    first commit

---
+++ Integracao_Parceria/Integracao.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Integracao_Parceria
+{
+    partial class Integracao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pesquisaTxt = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabelaShHomologadas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabelaShHomologadas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pesquisaTxt
+            // 
+            this.pesquisaTxt.Location = new System.Drawing.Point(2, 1);
+            this.pesquisaTxt.Name = "pesquisaTxt";
+            this.pesquisaTxt.Size = new System.Drawing.Size(312, 20);
+            this.pesquisaTxt.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(315, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 20);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabelaShHomologadas
+            // 
+            this.tabelaShHomologadas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabelaShHomologadas.Location = new System.Drawing.Point(3, 26);
+            this.tabelaShHomologadas.Name = "tabelaShHomologadas";
+            this.tabelaShHomologadas.Size = new System.Drawing.Size(344, 232);
+            this.tabelaShHomologadas.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 261);
+            this.Controls.Add(this.tabelaShHomologadas);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pesquisaTxt);
+            this.Name = "Form1";
+            this.Text = "Home homologa";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabelaShHomologadas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pesquisaTxt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView tabelaShHomologadas;
+    }
+}
+
